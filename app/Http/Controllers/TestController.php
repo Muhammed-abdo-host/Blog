16cda1e5ebadabadd2a3f20a7fd47ddcd2df732e
Kadwer <mohamedabdo01021570543@gmail.com>
2026-09-19 16:34:02 +0300

    updated

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function TestActions()
+    {
+         $posts = [
+        ['id' => '1', 'title' => 'laravel', 'posted_by' => 'Ahmed', 'cerated_at' => '2023-9-7'],
+        ['id' => '2', 'title' => 'PHP', 'posted_by' => 'Muhameed', 'cerated_at' => '2023-9-4'],
+    ];
+    return view('test', [
+        'posts' => $posts
+
+    ]);
+    }
+}
