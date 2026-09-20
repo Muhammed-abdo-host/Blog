ba003137e448c2db7591e3c909d148174d77fb28
Kadwer <mohamedabdo01021570543@gmail.com>
2026-09-20 13:03:29 +0300

    Add Show Method

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>show</title>
+</head>
+<body>
+    <div class="card">
+  <h5 class="card-header">Show post Details</h5>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+  </div>
+</div>
+</body>
+</html>
