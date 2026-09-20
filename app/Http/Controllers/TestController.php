2a00f33c04d734ce8e438373b0b7764413c34eb9
Kadwer <mohamedabdo01021570543@gmail.com>
2026-09-20 12:29:30 +0300

    Renameing Controllers

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,13 +6,13 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    public function TestActions()
+    public function index()
     {
          $posts = [
         ['id' => '1', 'title' => 'laravel', 'posted_by' => 'Ahmed', 'cerated_at' => '2023-9-7'],
         ['id' => '2', 'title' => 'PHP', 'posted_by' => 'Muhameed', 'cerated_at' => '2023-9-4'],
     ];
-    return view('test', [
+    return view('posts.index', [
         'posts' => $posts
 
     ]);
